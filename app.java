bb357049ffe2efbe940d81e02eac5dd641a8c19b
SharmilaG751 <gsharmilavijay2008@gmail.com>
2026-06-17 19:36:50 +0530

    first commit

---
+++ app.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.sql.*;
+
+public class Awtcla extends Frame{
+    Label l1,l2,l3,l4,l5,l6,l7,l8;
+    TextField t1,t2;
+    TextArea a,a1;
+    Choice c1,c2;
+    Checkbox rb1,rb2;
+    CheckboxGroup cg;
+    MenuItem m1,m2,m3,m4;
+    File cf,nf;
+    String filename;
+
+    
+}
